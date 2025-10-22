c9d009ef6c6672b7ac320dce12745a79115a2cce
andrewzeidell <andrewzeidell@gmail.com>
2025-10-21 22:54:10 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use cpu::CpuBackend;
 use vk::VkBackend;
 
 fn main() -> anyhow::Result<()> {
-    let rows = 100;
-    let cols = 200;
+    let rows = 32;
+    let cols = 32;
     let mut data = vec![
         1.0, 2.0, 3.0, 4.0, 1.0, 2.0, 3.0, 4.0,
         5.0, 4.0, 3.0, 2.0, 5.0, 4.0, 3.0, 2.0,
